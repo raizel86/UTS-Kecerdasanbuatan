f5b4e8d355433eac1a4fa33fed639e5a80b5e7f5
raizel86 <rio.azriel@gmail.com>
2022-04-19 15:59:23 +0700

    Menambakan besar nilai dengan teknik shorthand

--- utsPengkondisian.py
+++ utsPengkondisian.py
@@ -1,3 +1,4 @@
+# untuk memasukan apakah nilai lebih besar atau kecil
 print("Masukan 2 buah Nilai")
 a = int(input('bilangan a : '))
 b = int(input('bilangan b : '))
@@ -11,3 +12,8 @@ print("Menentukan Bilangan ganjil & genap")
 bilangan = int(input('Masukan Bilangan: '))
 print('nilai', bilangan, 'adalah bilangan',
       'genap' if (bilangan % 2 == 0) else 'ganjil')
+
+# mengetahui besar nilai dengan teknik shorthand
+nilai = int(input('Nilai Ujian : '))
+print("Grade A") if nilai >= 90 else print("Grade B") if nilai >= 75 else print(
+    "Grade C") if nilai >= 55 else print("Grade D")
